fd763bf231640815dabe39c708190720c8267eb7
nvm <nevermn@protonmail.com>
2020-04-06 14:03:53 +0300

    Move stock init logic to main

--- cmd/stock/main.go
+++ cmd/stock/main.go
@@ -1,7 +1,28 @@
 package main
 
-import stock "nvm.ga/mastersofcode/golang_2019/stock_distributed/internal/stock"
+import (
+	"context"
+
+	wh "nvm.ga/mastersofcode/golang_2019/stock_distributed/internal/stock/warehouse"
+	"nvm.ga/mastersofcode/golang_2019/stock_distributed/internal/stock/web"
+)
 
 func main() {
-	stock.StartService()
+	addressBook := wh.MakeAddressBook()
+	ctx, _ := context.WithCancel(context.Background())
+	go wh.DiscoverWarehouses(addressBook)
+	go web.StartServer(ctx, "8001", addressBook)
+
+	// todo: listen to cancellation signals
+	done := make(chan struct{}, 1)
+	<-done
 }
+
+// todo maybe add method to check which warehouses are still
+// alive
+// maybe make an infinite loop that will periodically check on every warehouse
+// and remove those that are dead
+
+// todo: add orders and order status checking
+
+// todo: add order persistence
